2fcd6adc2c1341444cdd124ef481056b3dd98d95
Alvarojrzz <juarezgarciaalvaro@gmail.com>
2025-04-04 00:45:57 +0200

    New web

---
+++ src/data/recetas.ts
@@ -0,0 +1,147 @@
+export const recetas = [
+        {
+        titulo: "Yogurt Cajun Dip",
+        descripcion: "Un dip cremoso y picante perfecto para acompañar vegetales o snacks saludables.",
+        imagen: "/images/Recetas/yogurtcajun.jpeg",
+        calories: "50 kcal",
+        proteina: "3 g",
+        grasas: "2 g",
+        carbohidratos: "4 g"
+      },
+      {
+        titulo: "Sweet Potato Brownies",
+        descripcion: "Deliciosos brownies veganos y sin gluten hechos con batata, ideales para un postre saludable.",
+        imagen: "/images/Recetas/SweetPotatoBrownie.jpeg",
+        calories: "110 kcal",
+        proteina: "2 g",
+        grasas: "3 g",
+        carbohidratos: "20 g"
+      },
+      {
+        titulo: "Chipotle Marinated Steak Burrito Bowl",
+        descripcion: "Un bowl nutritivo con carne marinada al chipotle, arroz y vegetales frescos.",
+        imagen: "/images/Recetas/ChipotleMarinatedSteakBurritoBow.jpeg",
+        calories: "470 kcal",
+        proteina: "40 g",
+        grasas: "15 g",
+        carbohidratos: "45 g"
+      },
+      {
+        titulo: "Beef Teriyaki Rice and Stir Fry",
+        descripcion: "Tierno ternera en salsa teriyaki servido con arroz y vegetales salteados.",
+        imagen: "/images/Recetas/BeefTeriyakiRiceandStirFr.jpeg",
+        calories: "520 kcal",
+        proteina: "45 g",
+        grasas: "12 g",
+        carbohidratos: "60 g"
+      },
+      {
+        titulo: "Chicken Teriyaki Rice and Broccoli",
+        descripcion: "Jugoso pollo en salsa teriyaki acompañado de arroz y brócoli al vapor.",
+        imagen: "/images/Recetas/ChickenTeriyakiRiceandBroccol.jpeg",
+        calories: "480 kcal",
+        proteina: "50 g",
+        grasas: "8 g",
+        carbohidratos: "55 g"
+      },
+      {
+        titulo: "Braised Pork with Soy Rice and Coleslaw",
+        descripcion: "Cerdo estofado servido con arroz al soja y ensalada de col.",
+        imagen: "/images/Recetas/BraisedPorithSoyRiceandColesla.jpeg",
+        calories: "600 kcal",
+        proteina: "55 g",
+        grasas: "20 g",
+        carbohidratos: "50 g"
+      },
+      {
+        titulo: "Buffalo Chicken Thigh with Mashed Potatoes",
+        descripcion: "Muslos de pollo al estilo búfalo acompañados de puré de papas cremoso.",
+        imagen: "/images/Recetas/BuffaloChickenThighwithMashedPotatoes.jpeg",
+        calories: "550 kcal",
+        proteina: "48 g",
+        grasas: "18 g",
+        carbohidratos: "50 g"
+      },
+      {
+        titulo: "Southern-Style Creole Chicken",
+        descripcion: "Pollo al estilo criollo sureño con una mezcla de especias y vegetales.",
+        imagen: "/images/Recetas/Southern-StyleCreoleChicke.jpeg",
+        calories: "450 kcal",
+        proteina: "42 g",
+        grasas: "10 g",
+        carbohidratos: "40 g"
+      },
+      {
+        titulo: "Curried Crispy Chickpeas",
+        descripcion: "Garbanzos crujientes al curry, perfectos como snack o para añadir a ensaladas.",
+        imagen: "/images/Recetas/CurriedCrispyChickpea.jpeg",
+        calories: "150 kcal",
+        proteina: "6 g",
+        grasas: "5 g",
+        carbohidratos: "20 g"
+      },
+      {
+        titulo: "Jasmine Rice with Fajita Veggies",
+        descripcion: "Arroz jazmín acompañado de vegetales al estilo fajita.",
+        imagen: "/images/Recetas/JasmineRicewithFajitaVeggie.jpeg",
+        calories: "350 kcal",
+        proteina: "8 g",
+        grasas: "4 g",
+        carbohidratos: "70 g"
+      },
+      {
+        titulo: "Skillet Lasagna",
+        descripcion: "Lasaña preparada en sartén, una versión rápida y deliciosa del clásico italiano.",
+        imagen: "/images/Recetas/SkilletLasagna.jpeg",
+        calories: "400 kcal",
+        proteina: "30 g",
+        grasas: "12 g",
+        carbohidratos: "45 g"
+      },
+      {
+        titulo: "Slow Cooker Chicken and Lentil Soup",
+        descripcion: "Sopa reconfortante de pollo y lentejas cocida a fuego lento.",
+        imagen: "/images/Recetas/SlowCookerChickenandLentilSou.jpeg",
+        calories: "300 kcal",
+        proteina: "35 g",
+        grasas: "5 g",
+        carbohidratos: "30 g"
+      },
+      {
+        titulo: "Sheet Pan Baked Feta with Broccolini, Tomatoes and Chickpeas",
+        descripcion: "Feta horneado con broccolini, tomates y garbanzos, todo en una bandeja.",
+        imagen: "/images/Recetas/SheetPanBakedFetawithBroccolini,TomatoesandChickpeas.jpeg",
+        calories: "380 kcal",
+        proteina: "18 g",
+        grasas: "20 g",
+        carbohidratos: "35 g"
+      },
+      {
+        titulo: "One-Pot Orzo with Spinach and Feta",
+        descripcion: "Orzo cremoso cocido en una sola olla con espinacas y queso feta.",
+        imagen: "/images/Recetas/One-PotOrzowithSpinachandFet.jpeg",
+        calories: "420 kcal",
+        proteina: "22 g",
+        grasas: "10 g",
+        carbohidratos: "60 g"
+      },
+      {
+        titulo: "Quinoa Lentil Berry Salad",
+        descripcion: "Ensalada fresca de quinoa, lentejas y bayas, rica en proteínas y antioxidantes.",
+        imagen: "/images/Recetas/QuinoaLentilBerrySalad.jpeg",
+        calories: "320 kcal",
+        proteina: "12 g",
+        grasas: "8 g",
+        carbohidratos: "50 g"
+      },
+      {
+        titulo: "Thai-Style Chicken Salad",
+        descripcion: "Ensalada de pollo al estilo tailandés con aderezo de cacahuate.",
+        imagen: "/images/Recetas/Thai-StyleChickenSala.jpeg",
+        calories: "350 kcal",
+        proteina: "40 g",
+        grasas: "12 g",
+        carbohidratos: "20 g"
+      }
+  ];
+  
